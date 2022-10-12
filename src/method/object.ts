36ececcb507401d3d003ee5423a20abf95f6e774
John Denver <chogathkk@gmail.com>
2022-10-12 11:09:26 +0800

    feat: optimize the efficiency of depth traversal and add to deep save method

--- src/method/object.ts
+++ src/method/object.ts
@@ -7,24 +7,24 @@ import { isValid, isValidDict, isValidString } from './validate';
  * Walks deeply through the object and returns the property value corresponding to the specified parameter.
  *
  * @param obj The object to judge.
- * @param token The properties of the object and splice them with `rule`.
+ * @param key The properties of the object and splice them with `rule`.
  * @param rule The rules for splicing, by default, are `.`.
  * @returns T
  *
  * @example
  * ```ts
- *  toDeepMatch({ job: { name: 'job' }}, 'job.name') // 'job'
+ *  toDeepMatch({ worker: { name: 'Chogath' }}, 'worker.name') // 'Chogath'
  * ```
  *
  * @publicApi
  */
-export function toDeepMatch<T = any>(obj: any, token: string, rule = '.'): T {
+export function toDeepMatch<T = any>(obj: object, key: string, rule = '.'): T {
   if (!isValidString(rule) || !isValidDict(obj)) return null;
 
   const stack = [];
-  const factors = token.split(rule);
+  const factors = key.split(rule);
 
-  let node;
+  let node: any;
   let nodeResult = null;
   let nodeDeepLevel = 0;
 
@@ -33,19 +33,21 @@ export function toDeepMatch<T = any>(obj: any, token: string, rule = '.'): T {
   while (stack.length > 0) {
     node = stack.pop();
 
-    if (nodeDeepLevel === factors.length) {
-      nodeResult = node;
-      break;
-    }
-
     if (isValidDict(node)) {
       for (const key of Object.keys(node)) {
         const indexResult = factors.indexOf(key);
         const factorResult = factors[nodeDeepLevel];
 
         if (key === factorResult && indexResult === nodeDeepLevel) {
-          stack.push(node[key]);
           nodeDeepLevel += 1;
+
+          if (nodeDeepLevel === factors.length) {
+            nodeResult = node[key];
+            break;
+          }
+
+          stack.push(node[key]);
+
           break;
         }
       }
@@ -55,25 +57,76 @@ export function toDeepMatch<T = any>(obj: any, token: string, rule = '.'): T {
   return nodeResult;
 }
 /**
- * Restores to the corresponding object based on the depth of the specified property and property value.
+ * Traverses through the object and modifies the property values corresponding to the specified parameters.
  *
+ * @param obj The object to judge.
  * @param value The object properties value.
- * @param token The properties of the object and splice them with `rule`.
+ * @param key The properties of the object and splice them with `rule`.
  * @param rule The rules for splicing, by default, are `.`.
  * @returns T
  *
  * @example
  * ```ts
- *  toDeepRestore('job', 'job.name') // { job: { name: 'job' }}
+ *  toDeepMatch({ worker: { name: 'Chogath' }}, 'worker.name') // 'Chogath'
  * ```
  *
  * @publicApi
  */
-export function toDeepRestore<T = object>(value: any, token: string, rule = '.'): T {
-  if (isNil(token) || !isValidString(token)) return null;
+export function toDeepSave<T = object>(obj: T, value: any, key: string, rule = '.'): void {
+  if (!isValidString(rule) || !isValidDict(obj)) return null;
+
+  const stack = [];
+  const factors = key.split(rule);
+
+  let node: any;
+  let nodeDeepLevel = 0;
+
+  stack.push(obj);
+
+  while (stack.length > 0) {
+    node = stack.pop();
+
+    if (isValidDict(node)) {
+      for (const key of Object.keys(node)) {
+        const indexResult = factors.indexOf(key);
+        const factorResult = factors[nodeDeepLevel];
+
+        if (key === factorResult && indexResult === nodeDeepLevel) {
+          nodeDeepLevel += 1;
+
+          if (nodeDeepLevel === factors.length) {
+            node[key] = value;
+            break;
+          }
+
+          stack.push(node[key]);
+
+          break;
+        }
+      }
+    }
+  }
+}
+/**
+ * Restores to the corresponding object based on the depth of the specified property and property value.
+ *
+ * @param value The object properties value.
+ * @param key The properties of the object and splice them with `rule`.
+ * @param rule The rules for splicing, by default, are `.`.
+ * @returns T
+ *
+ * @example
+ * ```ts
+ *  toDeepRestore('Chogath', 'worker.name') // { worker: { name: 'Chogath' }}
+ * ```
+ *
+ * @publicApi
+ */
+export function toDeepRestore<T = object>(value: any, key: string, rule = '.'): T {
+  if (isNil(key) || !isValidString(key)) return null;
 
   const object = Object();
-  const factors = token.split(rule);
+  const factors = key.split(rule);
 
   let node = object;
 
